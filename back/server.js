a1435a09698531833657577f7939345729171857
maya.shasho <maya.shasho6292@gmail.com>
2025-03-03 17:38:27 +0200

    add  /Product_Management_Application

--- back/server.js
+++ back/server.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/auth', authRoutes);
-app.use('/products', productRoutes);
+app.use('/Product_Management_Application/auth', authRoutes);
+app.use('/Product_Management_Application/products', productRoutes);
 app.use(errorHandler);
 
 const PORT = process.env.PORT;
