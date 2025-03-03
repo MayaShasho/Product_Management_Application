e4a976282007d9be2572788b6e7599e983ca2927
maya.shasho <maya.shasho6292@gmail.com>
2025-03-03 15:39:07 +0200

    Updates

---
+++ precache-manifest.bbd7cfc90258a94d1c5ee1f77d685688.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec5aebf1f1516630e4714ea24a25554",
+    "url": "/Product_Management_Application/index.html"
+  },
+  {
+    "revision": "c6239a7b8c7029b670b3",
+    "url": "/Product_Management_Application/static/css/main.949afc3d.chunk.css"
+  },
+  {
+    "revision": "309bfa15e51dbb5e9546",
+    "url": "/Product_Management_Application/static/js/2.7d21e2b2.chunk.js"
+  },
+  {
+    "revision": "729a8b5e56b3f3d0b9ae38e314606dcc",
+    "url": "/Product_Management_Application/static/js/2.7d21e2b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6239a7b8c7029b670b3",
+    "url": "/Product_Management_Application/static/js/main.d388cc65.chunk.js"
+  },
+  {
+    "revision": "6a8116c8eb6008220a12",
+    "url": "/Product_Management_Application/static/js/runtime-main.be9642fe.js"
+  }
+]);
