0d59ad303f963976b92482ea8b7fa597733da082
Maya Shasho <mayaaau@gmail.com>
2026-08-03 11:09:02 +0300

    Updates

---
+++ precache-manifest.3f74fd9c1dd7a161f385766465cf20d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db074d76fbdf9403bccd64cf8d03d93",
+    "url": "/Product_Management_Application/index.html"
+  },
+  {
+    "revision": "23a1c74680708482ca8a",
+    "url": "/Product_Management_Application/static/css/main.da1efc81.chunk.css"
+  },
+  {
+    "revision": "2e2d6f0dd0bd1116d5b1",
+    "url": "/Product_Management_Application/static/js/2.18c11979.chunk.js"
+  },
+  {
+    "revision": "729a8b5e56b3f3d0b9ae38e314606dcc",
+    "url": "/Product_Management_Application/static/js/2.18c11979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a1c74680708482ca8a",
+    "url": "/Product_Management_Application/static/js/main.7b2f3a28.chunk.js"
+  },
+  {
+    "revision": "6a8116c8eb6008220a12",
+    "url": "/Product_Management_Application/static/js/runtime-main.be9642fe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Product_Management_Application/precache-manifest.6e641ef8fac90a35f8625e3a9607d7b6.js"
+  "/Product_Management_Application/precache-manifest.3f74fd9c1dd7a161f385766465cf20d3.js"
 );
 
 self.addEventListener('message', (event) => {
